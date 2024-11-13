79e872e8740fe9957ffe9a8847cdb314bc46627c
csprashantraj <prashant77399@gmail.com>
2024-11-13 21:10:54 +0530

    added uds, send_to, recv to

---
+++ Basic/UDP.server.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <fcntl.h>
+
+#define SOCKET_PATH "/tmp/uds_socket"
+
+// Function to send a file descriptor
+int send_fd(int socket, int fd_to_send)
+ {
+  struct msghdr socket_message;
+  struct iovec io_vector[1];
+  struct cmsghdr *control_message = NULL;
+  char message_buffer[1];
+  /* storage space needed for an ancillary element with a paylod of length is CMSG_SPACE(sizeof(length)) */
+  char ancillary_element_buffer[CMSG_SPACE(sizeof(int))];
+  int available_ancillary_element_buffer_space;
+
+  /* at least one vector of one byte must be sent */
+  message_buffer[0] = 'F';
+  io_vector[0].iov_base = message_buffer;
+  io_vector[0].iov_len = 1;
+
+  /* initialize socket message */
+  memset(&socket_message, 0, sizeof(struct msghdr));
+  socket_message.msg_iov = io_vector;
+  socket_message.msg_iovlen = 1;
+
+  /* provide space for the ancillary data */
+  available_ancillary_element_buffer_space = CMSG_SPACE(sizeof(int));
+  memset(ancillary_element_buffer, 0, available_ancillary_element_buffer_space);
+  socket_message.msg_control = ancillary_element_buffer;
+  socket_message.msg_controllen = available_ancillary_element_buffer_space;
+
+  /* initialize a single ancillary data element for fd passing */
+  control_message = CMSG_FIRSTHDR(&socket_message);
+  control_message->cmsg_level = SOL_SOCKET;
+  control_message->cmsg_type = SCM_RIGHTS;
+  control_message->cmsg_len = CMSG_LEN(sizeof(int));
+  *((int *) CMSG_DATA(control_message)) = fd_to_send;
+
+  return sendmsg(socket, &socket_message, 0);
+ }
+ int recv_fd(int socket)
+ {
+  int sent_fd, available_ancillary_element_buffer_space;
+  struct msghdr socket_message;
+  struct iovec io_vector[1];
+  struct cmsghdr *control_message = NULL;
+  char message_buffer[1];
+  char ancillary_element_buffer[CMSG_SPACE(sizeof(int))];
+
+  /* start clean */
+  memset(&socket_message, 0, sizeof(struct msghdr));
+  memset(ancillary_element_buffer, 0, CMSG_SPACE(sizeof(int)));
+
+  /* setup a place to fill in message contents */
+  io_vector[0].iov_base = message_buffer;
+  io_vector[0].iov_len = 1;
+  socket_message.msg_iov = io_vector;
+  socket_message.msg_iovlen = 1;
+
+  /* provide space for the ancillary data */
+  socket_message.msg_control = ancillary_element_buffer;
+  socket_message.msg_controllen = CMSG_SPACE(sizeof(int));
+
+  if(recvmsg(socket, &socket_message, MSG_CMSG_CLOEXEC) < 0)
+   return -1;
+
+  if(message_buffer[0] != 'F')
+  {
+   /* this did not originate from the above function */
+   return -1;
+  }
+
+  if((socket_message.msg_flags & MSG_CTRUNC) == MSG_CTRUNC)
+  {
+   /* we did not provide enough space for the ancillary element array */
+   return -1;
+  }
+
+  /* iterate ancillary elements */
+   for(control_message = CMSG_FIRSTHDR(&socket_message);
+       control_message != NULL;
+       control_message = CMSG_NXTHDR(&socket_message, control_message))
+  {
+   if( (control_message->cmsg_level == SOL_SOCKET) &&
+       (control_message->cmsg_type == SCM_RIGHTS) )
+   {
+    sent_fd = *((int *) CMSG_DATA(control_message));
+    return sent_fd;
+   }
+  }
+
+  return -1;
+ }
+
+
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_un addr;
+
+    // Create socket
+    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Remove any existing socket file
+    unlink(SOCKET_PATH);
+
+    // Bind the socket
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 5) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server waiting for connection...\n");
+
+    // Accept a client connection
+    client_fd = accept(server_fd, NULL, NULL);
+    if (client_fd < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Client connected, sending file descriptor...\n");
+
+    // Open the file to send
+    int fd_to_send = open("test.txt", O_RDONLY);
+    if (fd_to_send < 0) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Send the file descriptor
+    send_fd(client_fd, fd_to_send);
+
+    close(fd_to_send);
+    close(client_fd);
+    close(server_fd);
+    unlink(SOCKET_PATH);
+
+    return 0;
+}
